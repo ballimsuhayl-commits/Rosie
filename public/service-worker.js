062c14d5f9a44928f476197030efcdc69fdb771e
ballimsuhayl-commits <ballimsuhayl@gmail.com>
2026-01-22 17:44:20 +0200

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,17 @@
-const CACHE_NAME = 'rosie-cache-v1';
+const CACHE_NAME = 'rosie-v5-baseline';
 const urlsToCache = ['/', '/index.html', '/static/js/bundle.js'];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(urlsToCache)));
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((ks) => {
+    return Promise.all(ks.map((k) => { if (k !== CACHE_NAME) return caches.delete(k); }));
+  }));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
