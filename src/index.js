21e737f4efcf7c72a40ebaf4c8c20bf924cb012c
ballimsuhayl-commits <ballimsuhayl@gmail.com>
2026-01-24 18:45:15 +0200

    Remove comment in index.js
    Removed comment about creating the root element.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// Create the root element and render the application.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
